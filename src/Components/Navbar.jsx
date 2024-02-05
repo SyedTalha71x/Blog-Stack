2ef48444c4a821daff63f00d4fca1aefe92685a8
SyedTalha71x <syedtalha71x@gmail.com>
2024-02-05 23:49:18 +0500

    Initial Start

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,105 @@
+import React, { useState } from 'react'
+import Link from 'next/link';
+
+const Navbar = (props) => {
+
+
+    const [isNavOpen, setIsNavOpen] = useState(false);
+    return (
+        <div className="flex items-center justify-around bg-slate-50 p-4 sticky top-0 cursor-pointer" style={{ backgroundColor: props.mode === 'light' ? '' : '#1C0502' }}>
+            <h1 style={{ color: props.mode === 'light' ? '' : 'white' }} className='main_head text-3xl font-bold uppercase tracking-wider '>
+                Bl<span className='text-red-600'>o</span>gs
+            </h1>
+
+            <nav>
+                <section className="MOBILE-MENU flex lg:hidden">
+                    <div
+                        className="HAMBURGER-ICON space-y-2"
+                        onClick={() => setIsNavOpen((prev) => !prev)}
+                    >
+                        <span className="block h-0.5 w-8 animate-pulse bg-gray-900"></span>
+                        <span className="block h-0.5 w-8 animate-pulse bg-gray-900"></span>
+                        <span className="block h-0.5 w-8 animate-pulse bg-gray-900"></span>
+                    </div>
+
+                    <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+                        <div
+                            className="absolute top-0 right-0 px-8 py-8"
+                            onClick={() => setIsNavOpen(false)}
+                        >
+                            <svg
+                                className="h-8 w-8 text-gray-600"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            >
+                                <line x1="18" y1="6" x2="6" y2="18" />
+                                <line x1="6" y1="6" x2="18" y2="18" />
+                            </svg>
+                        </div>
+                        <ul className="flex flex-col items-center justify-between min-h-[250px]">
+                            <li className="border-b border-gray-400 my-8 uppercase">
+                                <Link style={{ color: props.mode === 'light' ? '' : 'white' }} href={"/about"}>About</Link>
+                            </li>
+                            <li className="border-b border-gray-400 my-8 uppercase">
+                                <Link style={{ color: props.mode === 'light' ? '' : 'white' }} href={"/"}>Homepage</Link>
+                            </li>
+                            <li className="border-b border-gray-400 my-8 uppercase">
+                                <Link style={{ color: props.mode === 'light' ? '' : 'white' }} href={"/contact"}>Contact</Link>
+                            </li>
+                            <li className="border-b border-gray-400 my-8 uppercase">
+                                <Link style={{ color: props.mode === 'light' ? '' : 'white' }} href={"/login"}>Login</Link>
+                            </li>
+                            <li>
+                                <a style={{ color: props.mode === 'light' ? '' : 'black', backgroundColor: props.mode === 'light' ? '' : 'lightblue' }} onClick={props.togglemode} className='text-[15px]  bg-black text-white py-2 px-5  rounded-md '>Dark mode</a>
+                            </li>
+                        </ul>
+                    </div>
+                </section>
+
+                <ul className="DESKTOP-MENU hidden space-x-8 lg:flex">
+                    <li>
+                        <Link style={{ color: props.mode === 'light' ? '' : 'white' }} className='text-[17px] font-semibold hover:underline underline-offset-4  rounded-md ' href={"/about"}>About</Link>
+                    </li>
+                    <li>
+                        <Link style={{ color: props.mode === 'light' ? '' : 'white' }} className='text-[17px] font-semibold hover:underline underline-offset-4   rounded-md ' href={"/"}>Homepage</Link>
+                    </li>
+                    <li>
+                        <Link style={{ color: props.mode === 'light' ? '' : 'white' }} className='text-[17px] font-semibold hover:underline underline-offset-4   rounded-md ' href={"/contact"}>Contact</Link>
+                    </li>
+                    <li>
+                        <Link style={{ color: props.mode === 'light' ? '' : 'white' }} className='text-[17px] font-semibold hover:underline underline-offset-4   rounded-md ' href={"/login"}>Login</Link>
+                    </li>
+                    <li>
+                        <a style={{ color: props.mode === 'light' ? '' : 'black', backgroundColor: props.mode === 'light' ? '' : 'lightblue' }} onClick={props.togglemode} className='text-[15px]  bg-black text-white py-2 px-5  rounded-md '>Dark mode</a>
+                    </li>
+                </ul>
+            </nav>
+            <style>{`
+        .hideMenuNav {
+          display: none;
+        }
+        .showMenuNav {
+          display: block;
+          position: absolute;
+          font-weight: bold;
+          width: 100%;
+          height: 100vh;
+          top: 0;
+          left: 0;
+          background: #E9DADA;
+          z-index: 10;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-evenly;
+          align-items: center;
+        }
+      `}</style>
+        </div>
+    )
+}
+
+export default Navbar
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,26 @@
+import Navbar from "@/Components/Navbar";
 import "@/styles/globals.css";
+import React, { useState } from "react";
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+
+  const [mode, setmode] = useState('light')
+
+  const togglemode = () => {
+    if (mode === 'light') {
+      setmode('dark');
+      document.body.style.backgroundColor = 'black';
+      // document.body.style.transition = ".4s ease-in-out";
+    }
+    else {
+      setmode('light');
+      document.body.style.backgroundColor = 'white';
+      // document.body.style.transition = ".4s ease-in-out";
+    }
+  }
+  return <>
+    <Navbar mode={mode} togglemode={togglemode} />
+    <Component mode={mode}  {...pageProps} />
+  </>;
 }
